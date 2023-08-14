42ab5fb2c6aa78d08e8ba85d89854a73ecfc2e8f
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-08-14 15:26:36 -0400

    admin button

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls, name='admin'),
     path("", include('writers.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
