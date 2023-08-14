00b85baf92bfb548b13751bfe3249e7ea361f2b2
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-08-14 15:07:48 -0400

    mysql server

--- website/settings.py
+++ website/settings.py
@@ -64,12 +64,25 @@ WSGI_APPLICATION = "website.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'writersblog',
+        'USER': 'writersblog',
+        'PASSWORD': '04082023',
+        'HOST': 'db4free.net',  # Ou o endereço do servidor MySQL
+        'PORT': '3306',       # A porta padrão do MySQL
+    }
+}
+
 
 
 # Password validation
